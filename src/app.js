cc7e977b5f7064225ee8462870b84c978583c082
tenclar <tenclarvalus@gmail.com>
2019-10-26 23:03:24 -0500

    autenticação usuário

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import './database';
+import Youch from 'youch';
 import routes from './routes';
 
 class App {
@@ -7,6 +9,7 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -15,6 +18,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
+import Student from '../app/models/Student';
 
-const models = [User];
+const models = [User, Student];
 
 class Database {
   constructor() {
@@ -11,6 +12,7 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
     models.map(model => model.init(this.connection));
   }
 }
