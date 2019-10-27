b2b57c6c1d8950401a9718812d39830ab8e99c51
tenclar <tenclarvalus@gmail.com>
2019-10-26 20:45:00 -0500

    Cadastro de Estudantes

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,31 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async index(req, res) {
+    const students = await Student.findAll();
+
+    return res.json(students);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      idade: Yup.number().required(),
+      peso: Yup.number().required(),
+      altura: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fields fails' });
+    }
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+}
+
+export default new StudentController();
