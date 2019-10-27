9747e313e248114e77da6c0d4c8717292150050f
tenclar <tenclarvalus@gmail.com>
2019-10-26 20:45:43 -0500

    rota para cadastro de estudante

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import StudentController from './app/controllers/StudentController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+routes.post('/sessions', SessionController.store);
+
+routes.get('/students', StudentController.index);
+routes.post('/students', StudentController.store);
 
 export default routes;
